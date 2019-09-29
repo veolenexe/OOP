7152540c85de3d24fb10fea3beb04530d2958dd6
mistertimechip <mistertimechip@mail.ru>
2019-09-29 21:07:19 +0500

    Bot is connected and answering

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,32 @@
 import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
 
-public class Main
+public class Main extends ListenerAdapter
 {
     public static void main(String[] args) throws LoginException
     {
         JDABuilder builder = new JDABuilder(AccountType.BOT);
-        String token = "NjI3Nzg5NTkwMzI5MTYzNzc2.XZDCHA.C8554Bt1eyt7iLuTZupAo41_1AI";
+        String token = "NjI3Nzg5NTkwMzI5MTYzNzc2.XZDPVg.eKvTF5u-XF5rCCzC0IuM_Espvho";
         builder.setToken(token);
+        builder.addEventListeners(new Main());
         builder.build();
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        System.out.println("We received a message from:" +
+                event.getAuthor().getName() + ": " +
+                event.getMessage().getContentDisplay()
+        );
+
+        if(event.getMessage().getContentRaw().equals("!ping"))
+        {
+            event.getChannel().sendMessage("Pong!").queue();
+        }
     }
 }
