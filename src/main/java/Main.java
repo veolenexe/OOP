5d0de34a12b8998ecd8e3bff7ae31afa2bf94f08
mistertimechip <mistertimechip@mail.ru>
2019-09-29 20:10:07 +0500

    IT'S ALIVE

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws LoginException
     {
-
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        String token = "NjI3Nzg5NTkwMzI5MTYzNzc2.XZDCHA.C8554Bt1eyt7iLuTZupAo41_1AI";
+        builder.setToken(token);
+        builder.build();
     }
 }
