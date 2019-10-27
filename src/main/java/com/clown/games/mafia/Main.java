6d8c5fe1c42b662f9475b19e09586b75fdda7fcf
veolenexe <44974960+veolenexe@users.noreply.github.com>
2019-10-27 22:18:50 +0500

    add private listener

--- src/main/java/com/clown/games/mafia/Main.java
+++ src/main/java/com/clown/games/mafia/Main.java
@@ -13,7 +13,7 @@ public class Main
     public static void main(String[] args) throws LoginException
     {
         JDABuilder builder = new JDABuilder(AccountType.BOT);
-        String token = "NjI3Nzg5NTkwMzI5MTYzNzc2.Xa8L2g.ZC_BWe7yaz13QfD-Trk9-QZAwPY";
+        String token = "";
         builder.setToken(token);
         DiscordMessageSender discordMessageSender = new DiscordMessageSender();
         DiscordMessageListener discordMessageListener = new DiscordMessageListener();
