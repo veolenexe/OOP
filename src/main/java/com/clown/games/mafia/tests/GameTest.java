1c467519409f39a2c8ccaab87cfeef1a48706374
Данил Панков <mistertimechip@mail.ru>
2019-11-24 14:22:11 +0300

    Added tests

---
+++ src/main/java/com/clown/games/mafia/tests/GameTest.java
@@ -0,0 +1,86 @@
+public class GameTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception
+    {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception
+    {
+
+    }
+
+    @org.junit.Test
+    public void prepareForGame()
+    {
+
+    }
+
+    @org.junit.Test
+    public void startGame()
+    {
+
+    }
+
+    @org.junit.Test
+    public void addMove()
+    {
+
+    }
+
+    @org.junit.Test
+    public void makeAVote()
+    {
+
+    }
+
+    @org.junit.Test
+    public void getPlayerByID()
+    {
+
+    }
+
+    @org.junit.Test
+    public void getPlayerByNumber()
+    {
+
+    }
+
+    @org.junit.Test
+    public void addParticipant()
+    {
+
+    }
+
+    @org.junit.Test
+    public void isPlayerParticipant()
+    {
+
+    }
+
+    @org.junit.Test
+    public void getCurrentGameState()
+    {
+
+    }
+
+    @org.junit.Test
+    public void getCurrentPlayersCount()
+    {
+
+    }
+
+    @org.junit.Test
+    public void setMessageSender()
+    {
+
+    }
+
+    @org.junit.Test
+    public void sendMessage()
+    {
+
+    }
+}
