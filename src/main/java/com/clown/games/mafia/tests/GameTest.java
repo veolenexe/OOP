6d260c2187117d4258e066e733e44ea7540ad83e
Данил Панков <mistertimechip@mail.ru>
2019-11-24 18:08:55 +0300

    Tests for Game

--- src/main/java/com/clown/games/mafia/tests/GameTest.java
+++ src/main/java/com/clown/games/mafia/tests/GameTest.java
@@ -1,81 +1,88 @@
+package com.clown.games.mafia.tests;
+
+import com.clown.games.mafia.Game;
+import com.clown.games.mafia.GameState;
+import com.clown.games.mafia.IGame;
+import  com.clown.games.mafia.tests.testClasses.TestPlayer;
+import  com.clown.games.mafia.tests.testClasses.TestMessageSender;
+import org.junit.Assert;
+
 public class GameTest {
 
+    private IGame game;
+    private TestPlayer testPlayer;
+    private TestMessageSender sender;
+
     @org.junit.Before
-    public void setUp() throws Exception
+    public void setUp()
     {
-
-    }
-
-    @org.junit.After
-    public void tearDown() throws Exception
-    {
-
+        game = new Game();
+        sender = new TestMessageSender();
+        game.setMessageSender(sender);
+        testPlayer = new TestPlayer("Gordon Freeman", "lolid", 1);
     }
 
     @org.junit.Test
-    public void prepareForGame()
+    public void prepareForGame_should_changeGameState()
     {
-
+        game.prepareForGame();
+        Assert.assertEquals(GameState.PREPARATION, game.getCurrentGameState());
     }
 
     @org.junit.Test
-    public void startGame()
+    public void startGame_should_changeGameStateToNight()
     {
-
+        game.startGame();
+        Assert.assertEquals(GameState.NIGHT, game.getCurrentGameState());
     }
 
     @org.junit.Test
-    public void addMove()
+    public void makeAVote_should_sendMessageOnWrongVote()
     {
-
+        game.addParticipant(testPlayer);
+        game.makeAVote("9",testPlayer.getPlayerID());
     }
 
     @org.junit.Test
-    public void makeAVote()
+    public void getPlayerByID_should_returnPlayerByID()
     {
-
+        game.addParticipant(testPlayer);
+        var optional = game.getPlayerByID(testPlayer.getPlayerID());
+        Assert.assertTrue(optional.isPresent());
     }
 
     @org.junit.Test
-    public void getPlayerByID()
+    public void getPlayerByNumber_should_returnPlayerByNumber()
     {
-
+        game.addParticipant(testPlayer);
+        var optional = game.getPlayerByNumber(testPlayer.getPlayerNumber());
+        Assert.assertTrue(optional.isPresent());
     }
 
     @org.junit.Test
-    public void getPlayerByNumber()
+    public void addParticipant_should_addPlayerToTheGame()
     {
-
+        game.addParticipant(testPlayer);
+        Assert.assertEquals(1, game.getCurrentPlayersCount());
     }
 
     @org.junit.Test
-    public void addParticipant()
+    public void isPlayerParticipant_should_returnTrue_when_playerIsParticipant()
     {
-
+        game.addParticipant(testPlayer);
+        Assert.assertTrue(game.isPlayerParticipant(testPlayer.getPlayerID()));
     }
 
     @org.junit.Test
-    public void isPlayerParticipant()
+    public void getCurrentGameState_should_returnOfflineState_when_gameWasNotStarted()
     {
-
+        Assert.assertEquals(GameState.OFFLINE, game.getCurrentGameState());
     }
 
     @org.junit.Test
-    public void getCurrentGameState()
+    public void getCurrentPlayersCount_should_return0_when_noPlayers()
     {
-
-    }
-
-    @org.junit.Test
-    public void getCurrentPlayersCount()
-    {
-
-    }
-
-    @org.junit.Test
-    public void setMessageSender()
-    {
-
+        Assert.assertEquals(0, game.getCurrentPlayersCount() );
     }
 
     @org.junit.Test
---
+++ src/main/java/com/clown/games/mafia/tests/testClasses/TestMessageSender.java
@@ -0,0 +1,18 @@
+package com.clown.games.mafia.tests.testClasses;
+
+import com.clown.games.mafia.messaging.IMessageSender;
+
+public class TestMessageSender implements IMessageSender
+{
+    private String lastMessage;
+    @Override
+    public void sendMessage(String message)
+    {
+        lastMessage = message;
+    }
+
+    public String getLastMessage()
+    {
+        return lastMessage;
+    }
+}
