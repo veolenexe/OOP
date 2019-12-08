e1fb699d8241bf03e9120170995140ab902ba727
Данил Панков <mistertimechip@mail.ru>
2019-12-08 20:54:22 +0300

    Token is now read from file

--- src/main/java/com/clown/games/mafia/Main.java
+++ src/main/java/com/clown/games/mafia/Main.java
@@ -7,18 +7,37 @@ import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDABuilder;
 
 import javax.security.auth.login.LoginException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 public class Main
 {
     public static void main(String[] args) throws LoginException
     {
         JDABuilder builder = new JDABuilder(AccountType.BOT);
-        String token = "";
+        String token = getToken();
         builder.setToken(token);
         DiscordMessageListener discordMessageListener = new DiscordMessageListener();
         MafiaMySQLDatabase database = new MafiaMySQLDatabase();
         DiscordMafiaBot discordMafiaBot = new DiscordMafiaBot(discordMessageListener, database);
         builder.addEventListeners(discordMafiaBot.getMessageListener());
         builder.build();
+    }
+
+    private static String getToken()
+    {
+        Properties props = new Properties();
+        Path dir = Paths.get("src/main/resources");
+        Path path = dir.resolve("token.properties");
+        try(InputStream in = Files.newInputStream(path)){
+            props.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return props.getProperty("token");
     }
 }
