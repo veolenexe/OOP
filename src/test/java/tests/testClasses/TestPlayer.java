d4e1ef04f44c1d64df202f1ba00322ad6f99ea2c
veolenexe <iljaskripchenko@gmail.com>
2019-12-08 21:50:05 +0500

    Tests added

--- src/test/java/tests/testClasses/TestPlayer.java
+++ src/test/java/tests/testClasses/TestPlayer.java
@@ -2,22 +2,17 @@ package tests.testClasses;
 
 import com.clown.games.mafia.player.Player;
 
-public class TestPlayer extends Player {
+public class TestPlayer extends Player
+{
 
-    private String lastPrivateMessage;
     public TestPlayer(String playerName, String playerID, int playerNumber)
     {
         super(playerName, playerID, playerNumber);
     }
 
-    public String getLastPrivateMessage()
-    {
-        return lastPrivateMessage;
-    }
 
     @Override
     public void sendPrivateMessage(String message)
     {
-        lastPrivateMessage = message;
     }
 }
